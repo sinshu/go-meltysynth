296f1e39f7abe997e32a0644c38551ed949c180c
田中信秋 <sinshu.d@gmail.com>
2022-08-09 23:26:59 +0900

    Add files.

--- meltysynth/sample_header.go
+++ meltysynth/sample_header.go
@@ -7,16 +7,16 @@ import (
 )
 
 type SampleHeader struct {
-	name            string
-	start           int32
-	end             int32
-	startLoop       int32
-	endLoop         int32
-	sampleRate      int32
-	originalPitch   uint8
-	pitchCorrection int8
-	link            uint16
-	sampleType      uint16
+	Name            string
+	Start           int32
+	End             int32
+	StartLoop       int32
+	EndLoop         int32
+	SampleRate      int32
+	OriginalPitch   uint8
+	PitchCorrection int8
+	Link            uint16
+	SampleType      uint16
 }
 
 func readSampleHeadersFromChunk(reader io.Reader, size int32) ([]*SampleHeader, error) {
@@ -36,7 +36,7 @@ func readSampleHeadersFromChunk(reader io.Reader, size int32) ([]*SampleHeader, 
 
 		header := new(SampleHeader)
 
-		header.name, err = readFixedLengthString(reader, 20)
+		header.Name, err = readFixedLengthString(reader, 20)
 		if err != nil {
 			return nil, err
 		}
@@ -46,63 +46,63 @@ func readSampleHeadersFromChunk(reader io.Reader, size int32) ([]*SampleHeader, 
 		if err != nil {
 			return nil, err
 		}
-		header.start = start
+		header.Start = start
 
 		var end int32
 		err = binary.Read(reader, binary.LittleEndian, &end)
 		if err != nil {
 			return nil, err
 		}
-		header.end = end
+		header.End = end
 
 		var startLoop int32
 		err = binary.Read(reader, binary.LittleEndian, &startLoop)
 		if err != nil {
 			return nil, err
 		}
-		header.startLoop = startLoop
+		header.StartLoop = startLoop
 
 		var endLoop int32
 		err = binary.Read(reader, binary.LittleEndian, &endLoop)
 		if err != nil {
 			return nil, err
 		}
-		header.endLoop = endLoop
+		header.EndLoop = endLoop
 
 		var sampleRate int32
 		err = binary.Read(reader, binary.LittleEndian, &sampleRate)
 		if err != nil {
 			return nil, err
 		}
-		header.sampleRate = sampleRate
+		header.SampleRate = sampleRate
 
 		var originalPitch uint8
 		err = binary.Read(reader, binary.LittleEndian, &originalPitch)
 		if err != nil {
 			return nil, err
 		}
-		header.originalPitch = originalPitch
+		header.OriginalPitch = originalPitch
 
 		var pitchCorrection int8
 		err = binary.Read(reader, binary.LittleEndian, &pitchCorrection)
 		if err != nil {
 			return nil, err
 		}
-		header.pitchCorrection = pitchCorrection
+		header.PitchCorrection = pitchCorrection
 
 		var link uint16
 		err = binary.Read(reader, binary.LittleEndian, &link)
 		if err != nil {
 			return nil, err
 		}
-		header.link = link
+		header.Link = link
 
 		var sampleType uint16
 		err = binary.Read(reader, binary.LittleEndian, &sampleType)
 		if err != nil {
 			return nil, err
 		}
-		header.sampleType = sampleType
+		header.SampleType = sampleType
 
 		headers[i] = header
 	}
