82d309e911702256f2a9992593cdadb020950ce8
田中信秋 <sinshu.d@gmail.com>
2023-01-07 20:39:37 +0900

    Render waveform.

--- main.go
+++ main.go
@@ -2,26 +2,39 @@ package main
 
 import (
 	"os"
+	"sync"
 	"time"
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/speaker"
 	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 
 	"github.com/sinshu/go-meltysynth/meltysynth"
 )
 
+var visWaveLen int = 512
+var width float64 = 1024
+var height float64 = 768
+
 type audioStream struct {
-	seq      *meltysynth.MidiFileSequencer
+	seq *meltysynth.MidiFileSequencer
+
 	leftBuf  []float32
 	rightBuf []float32
+
+	visWave      []float64
+	visWaveStart int
+	mu           sync.Mutex
 }
 
 func NewAudioStream(seq *meltysynth.MidiFileSequencer) *audioStream {
 	result := new(audioStream)
 	result.seq = seq
+	result.visWave = make([]float64, visWaveLen)
+	result.visWaveStart = 0
 	return result
 }
 
@@ -38,16 +51,37 @@ func (s *audioStream) Stream(samples [][2]float64) (n int, ok bool) {
 
 	s.seq.Render(s.leftBuf[0:sampleCount], s.rightBuf[0:sampleCount])
 
+	s.mu.Lock()
 	for i := range samples {
 		samples[i][0] = float64(s.leftBuf[i])
 		samples[i][1] = float64(s.rightBuf[i])
+
+		s.visWave[s.visWaveStart] = float64(s.leftBuf[i] + s.rightBuf[i])
+		s.visWaveStart++
+		if s.visWaveStart == visWaveLen {
+			s.visWaveStart = 0
+		}
 	}
+	s.mu.Unlock()
 
 	return sampleCount, true
 }
 
 func (s *audioStream) Err() error {
 	return nil
+}
+
+func (s *audioStream) getVisWave(data []float64) {
+	s.mu.Lock()
+	var p int = s.visWaveStart
+	for i := 0; i < visWaveLen; i++ {
+		data[i] = s.visWave[p]
+		p++
+		if p == visWaveLen {
+			p = 0
+		}
+	}
+	s.mu.Unlock()
 }
 
 func run() {
@@ -69,7 +103,7 @@ func run() {
 
 	cfg := pixelgl.WindowConfig{
 		Title:  "MIDI music playback!",
-		Bounds: pixel.R(0, 0, 1024, 768),
+		Bounds: pixel.R(0, 0, width, height),
 		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
@@ -77,13 +111,29 @@ func run() {
 		panic(err)
 	}
 
-	win.Clear(colornames.Skyblue)
-
+	stream := NewAudioStream(sequencer)
 	sr := beep.SampleRate(sampleRate)
 	speaker.Init(sr, sr.N(time.Second/10))
-	speaker.Play(NewAudioStream(sequencer))
+	speaker.Play(stream)
+
+	imd := imdraw.New(nil)
+	data := make([]float64, visWaveLen)
 
 	for !win.Closed() {
+		stream.getVisWave(data)
+		imd.Reset()
+		imd.Clear()
+		imd.Color = colornames.Lightblue
+		imd.EndShape = imdraw.NoEndShape
+		for i := 0; i < visWaveLen; i++ {
+			x := float64(i) / float64(visWaveLen) * width
+			y := (height/4)*data[i] + (height / 2)
+			imd.Push(pixel.V(x, y))
+		}
+		imd.Line(3)
+
+		win.Clear(colornames.Darkblue)
+		imd.Draw(win)
 		win.Update()
 	}
 
